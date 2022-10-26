b9bc6a1998b9a19aa2c73611b2668844f1067cba
emma-junior <emnzboy31@gmail.com>
2022-10-26 23:40:04 +0100

    shopnow changes

--- src/components/Shopnow.tsx
+++ src/components/Shopnow.tsx
@@ -5,20 +5,18 @@ const Shopnow = () => {
   return (
     <section className='shopnow'>
         <div className='adidas'>
-            <div className='adidas__imgwrapper'><img className='adidas-img' src='/assets/images/shopnow-two.jpg' alt='' /></div>
             <div className='adidas-content'>
-                <p className='adidas-content-title'>Lorem ipsum dolor sit amet</p>
-                <p className='adidas-content-para'>Sed ut perspiciatis unde omnis iste natus</p>
+                <p className='adidas-content-title'>Great price you can count on</p>
+                <p className='adidas-content-para'>We sell sneakers at the best rate</p>
                 <p className='adidas-content-start'>START FROM</p>
                 <p className='adidas-content-price'>$59.25</p>
                 <button className='adidas-content-btn'>Shop Now</button>
             </div>
         </div>
         <div className='nike'>
-            <div className='nike__imgwrapper'><img className='nike-img' src='/assets/images/shopnow.jpg' alt='' /></div>
             <div className='nike-content'>
-                <p className='nike-content-title'>Lorem ipsum dolor</p>
-                <p className='nike-content-para'>Sed ut perspiciatis unde</p>
+                <p className='nike-content-title'>Give your foot the best</p>
+                <p className='nike-content-para'>Shop with us now</p>
             </div>
         </div>
     </section>
