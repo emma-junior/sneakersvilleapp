08a42b5c55a8f2d4358d32d02b43cdf4adca7b75
emma-junior <emnzboy31@gmail.com>
2024-07-21 16:43:15 +0100

    fixed responsiveness issue on lg screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import Home from './pages/Home';
-import AppProvider from './hooks/Context';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
-import Details from './pages/Details';
-import Cart from './pages/Cart';
-import Products from './pages/Products';
-import ScrollToTop from './hooks/ScrollToTop';
-import Register from './pages/Register';
-import Success from './pages/Success';
-import Login from './pages/Login';
-import PrivateRoute from './components/PrivateRoute';
-import Profile from './pages/Profile';
-import "./index.css"
-import PaymentForm from './components/CheckoutBtns/PaymentForm';
-
+import React from "react";
+import Home from "./pages/Home";
+import AppProvider from "./hooks/Context";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Details from "./pages/Details";
+import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+import ScrollToTop from "./hooks/ScrollToTop";
+import Register from "./pages/Register";
+import Success from "./pages/Success";
+import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
+import PaymentForm from "./components/CheckoutBtns/PaymentForm";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
@@ -25,17 +22,17 @@ function App() {
           <ScrollToTop />
           <Toaster />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/details/:id' element={<Details />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/products' element={<Products />} />
-            <Route path='/login' element={<PrivateRoute/>}>
-              <Route path='/login' element={<Login />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<PrivateRoute />}>
+              <Route path="/login" element={<Login />} />
             </Route>
-            <Route path='/success' element={<Success />} />
-            <Route path='/pay-with-paystack' element={<PaymentForm />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/coming-soon' element={<Profile />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/pay-with-paystack" element={<PaymentForm />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/coming-soon" element={<Profile />} />
           </Routes>
         </AppProvider>
       </Router>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistedStore } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -19,5 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-
